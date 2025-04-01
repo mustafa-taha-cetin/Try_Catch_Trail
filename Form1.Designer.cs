b7c2108da4ac05e32ae0dc1dc07f3b3c21309395
MustafaTahaCetin <=>
2025-04-01 11:59:51 +0300

    codes

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Try_Catch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(243, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 40);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(243, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(304, 40);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Birinci sayı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 35);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "İkinci sayı :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 55);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 35);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sonuç :";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(334, 302);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(64, 35);
+            this.lblSonuc.TabIndex = 6;
+            this.lblSonuc.Text = "Null";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.ClientSize = new System.Drawing.Size(765, 524);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Corbel", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Try_Catch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // yapılmak isteneni dener eğer bir sorun çıkarsa catch bloğuna geçer
+
+                int s1, s2, sonuc;
+                s1 = Convert.ToInt32(textBox1.Text);
+                s2 = Convert.ToInt32(textBox2.Text);
+                sonuc = s1 * s2;
+
+                lblSonuc.Text = Convert.ToString(sonuc);
+
+            }
+            catch (Exception)
+            {
+                // try bloğunda herhangi bir problem çıktığında devreye girer
+                MessageBox.Show("Girilen değerlere dikkat ediniz","Uyarı",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+            finally
+            {
+                // finally bloğu her iki durumda da çalışır
+                MessageBox.Show("Finally çalıştı");
+            }
+            
+
+        }
+
+    }
+}
